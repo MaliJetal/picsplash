1644e17bc2085c141d97a7c8e7fc661349ea20a7
Jetal Mali <jetmali@publicisgroupe.net>
2022-11-22 22:59:48 +0530

    Fetch Images

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,41 @@
-import React from 'react';
+import React, { ReactElement, useEffect, useState } from 'react';
+import axios from 'axios';
 import './App.css';
-import {mainUrl, searchUrl} from './AppConstants';
-
-function App() {
+import {api_endpoints} from './AppConstants';
+type PokemonData = {
+  id: string
+  number: string
+  name: string
+  image: string
+  fetchedAt: string
+  attacks: {
+    special: Array<{
+      name: string
+      type: string
+      damage: number
+    }>
+  }
+} 
+const App: React.FC = () => {
+  let url = `${api_endpoints.mainUrl}${api_endpoints.client_id}`;
+  const [loading, setLoading] = useState(false);
+  const [photos, setPhotos] =   useState<any>([]);
+  const fetchImages = async () : Promise<string[] | any> => {
+    await axios.get<string[]>(url)
+      .then((response) =>{
+        return response.data;
+      })
+      .catch((e) => {
+        console.log(e);
+        throw e;
+      });
+  }
+  useEffect(()=> {
+    setLoading(true);
+    const result =  fetchImages() ;
+    setPhotos(result);
+    setLoading(false);
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
--- src/AppConstants.ts
+++ src/AppConstants.ts
@@ -1,5 +1,5 @@
-export const unsplash_API_endpoints = {
+export const api_endpoints = {
     mainUrl : "https://api.unsplash.com/photos/",
     searchUrl : "https://api.unsplash.com/search/photos/",
-    client_id :  `?client_id=${process.env.SECRET_ACCESS_KEY}`
+    client_id :  `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
 }
